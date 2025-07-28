2aa7df2557bf054ad8a32e4441f69437e4c7db2f
Hailton Costa <59211869+hailtoncosta@users.noreply.github.com>
2025-07-28 02:49:19 +0000

    nivel aventureiro batalha naval 4 correcao

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -18,19 +18,19 @@ void gerar_tabuleiro_aventureiro(int tabuleiro[10][10]) {
             tabuleiro[i][j] = 0;
     
     //Posiciona navos na horizontal
-    for (int j = 2; j < 6; j++)
+    for (int j = 2; j < 5; j++)
         tabuleiro[0][j] = 3;
 
     //Posiciona navos na vertical
-    for (int i = 2; i < 6; i++)
+    for (int i = 2; i < 5; i++)
         tabuleiro[i][0] = 3;
         
     //Posiciona navio na diagonal principal
-    for (int i = 0; i < 4; i++)
+    for (int i = 0; i < 3; i++)
         tabuleiro[i][i] = 3;
     
     //Posiciona navio na diagonal secundária
-    for (int i = 0; i < 4; i++)
+    for (int i = 0; i < 3; i++)
         tabuleiro[i][9 - i] = 3;
 }
 
