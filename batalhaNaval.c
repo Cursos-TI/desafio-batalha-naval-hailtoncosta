5616cba615c867f5f2993b921baada32053d441a
Hailton Costa <59211869+hailtoncosta@users.noreply.github.com>
2025-07-28 02:17:52 +0000

    nivel aventureiro batalha naval

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,40 +1,42 @@
 #include <stdio.h>
 
-// Desafio Batalha Naval - MateCheck
-// Este código inicial serve como base para o desenvolvimento do sistema de Batalha Naval.
-// Siga os comentários para implementar cada parte do desafio.
+//Exibição de matriz
+void exibir_matriz(int matriz[10][10], int tamanho) {
+    for (int i = 0; i < tamanho; i++) {
+        for (int j = 0; j < tamanho; j++) {
+            printf("%d", matriz[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+//Gerando tabuleiro
+void gerar_tabuleiro_aventureiro(int tabuleiro[10][10]) {
+    //Iniciando com 0
+    for (int i = 0; i < 10; i++) 
+        for (int j = 0; j < 10; j++) 
+            tabuleiro[i][j] = 0;
+            
+    //Posiciona navios na horizontal
+    for (int i = 2; i < 6; i++)
+        tabuleiro[i][0] = 3;
+        
+    //Posiciona navio na diagonal principal
+    for (int i = 0; i < 4; i++)
+        tabuleiro[i][i] = 3;
+        
+    //Posiciona navio na diagonal secundária
+    for (int i = 0; i < 4; i++)
+        tabuleiro[i][9 - i] = 3;
+}
+
 
 int main() {
-    // Nível Novato - Posicionamento dos Navios
-    // Sugestão: Declare uma matriz bidimensional para representar o tabuleiro (Ex: int tabuleiro[5][5];).
-    // Sugestão: Posicione dois navios no tabuleiro, um verticalmente e outro horizontalmente.
-    // Sugestão: Utilize `printf` para exibir as coordenadas de cada parte dos navios.
 
-    // Nível Aventureiro - Expansão do Tabuleiro e Posicionamento Diagonal
-    // Sugestão: Expanda o tabuleiro para uma matriz 10x10.
-    // Sugestão: Posicione quatro navios no tabuleiro, incluindo dois na diagonal.
-    // Sugestão: Exiba o tabuleiro completo no console, mostrando 0 para posições vazias e 3 para posições ocupadas.
-
-    // Nível Mestre - Habilidades Especiais com Matrizes
-    // Sugestão: Crie matrizes para representar habilidades especiais como cone, cruz, e octaedro.
-    // Sugestão: Utilize estruturas de repetição aninhadas para preencher as áreas afetadas por essas habilidades no tabuleiro.
-    // Sugestão: Exiba o tabuleiro com as áreas afetadas, utilizando 0 para áreas não afetadas e 1 para áreas atingidas.
-
-    // Exemplos de exibição das habilidades:
-    // Exemplo para habilidade em cone:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 1 1 1 1 1
-    
-    // Exemplo para habilidade em octaedro:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 0 0 1 0 0
-
-    // Exemplo para habilidade em cruz:
-    // 0 0 1 0 0
-    // 1 1 1 1 1
-    // 0 0 1 0 0
+    int tabuleiro[10][10];
+    gerar_tabuleiro_aventureiro(tabuleiro);
+    printf("Tabuleiro - Nivel Aventureiro: \n");
+    exibir_matriz(tabuleiro, 10);
 
     return 0;
 }
